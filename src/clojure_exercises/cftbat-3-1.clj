d3f798c1dd26aec1625c6507e937bd2905c31d06
Marcus Josefsson <mjosefs@users.noreply.github.com>
2020-06-26 23:59:03 +0200

    Add CFTBAT exercise 3.1 (ran through REPL)

---
+++ src/clojure_exercises/cftbat-3-1.clj
@@ -0,0 +1,6 @@
+(ns clojure-exercises.core
+  (:gen-class))
+  
+(vector (str "This " "is " 1 " sentence")
+        (list 2 (hash-set 1 2 2) "third element in the list")
+        (:a (hash-map :a "and the last in the vector" :b 4)))
