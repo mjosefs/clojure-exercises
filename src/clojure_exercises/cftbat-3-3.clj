85ffa8f379bbe26401e221045bb1b61200847eda
Marcus Josefsson <mjosefs@users.noreply.github.com>
2020-06-27 00:22:12 +0200

    Add CFTBAT exercise 3.3 (ran through REPL)

---
+++ src/clojure_exercises/cftbat-3-3.clj
@@ -0,0 +1,10 @@
+(ns clojure-exercises.core
+  (:gen-class))
+
+(defn dec-maker
+  "Returns function that subtracts the specified number"
+  [subtr]
+  (fn [number] (- number subtr)))
+
+(def dec9 (dec-maker 9))
+(dec9 10)
