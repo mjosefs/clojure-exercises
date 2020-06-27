6b6d3c168f89a112380e6da34ac948e63b50068f
Marcus Josefsson <mjosefs@users.noreply.github.com>
2020-06-27 12:31:40 +0200

    Add CFTBAT exercise 3.6 (ran through REPL)

---
+++ src/clojure_exercises/cftbat-3-6.clj
@@ -0,0 +1,30 @@
+(ns clojure-exercises.core
+  (:gen-class))
+
+(defn generate-directions
+  "Generate a list of no-parts - 1 variants of the body part 'directions', generically named"
+  [no-parts]
+  (map #(str "direction" % "-") (range 2 (+ no-parts 1))))
+
+(defn alien-add-parts
+  "Works like symmetrize-body-parts but for stranger bodies (assuming the 'asymmetric parts' are still 'left' parts)"
+  [part no-parts]
+  (map (fn [part-orientation] {:name (clojure.string/replace (:name part) #"^left-" part-orientation)
+    :size (:size part)}) (generate-directions no-parts)))
+
+(defn symmetrize-body-parts
+  "Expands an abbreviated, asymmetrical sequence of body parts to contain all actual body parts"
+  [asymmetric-body-parts no-parts]
+  (reduce (fn [final-parts part]
+              (into final-parts
+                (set (into [part] (alien-add-parts part no-parts))))) ; Using into as otherwise a vector of a map and list
+          []
+          asymmetric-body-parts))
+
+; Just some body parts
+(def asymmetric-body-parts
+  [{:name "left-hand" :size 1}
+  {:name "head" :size 3}
+  {:name "left-leg" :size 4}])
+
+(symmetrize-body-parts asymmetric-body-parts 3)
