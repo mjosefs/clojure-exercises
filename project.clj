1151fad62dbc882a6e6bfdfaa004b662692c0f39
Marcus Josefsson <mjosefs@users.noreply.github.com>
2020-07-07 00:31:41 +0200

    Add proto-repl dependency

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clojure-exercises "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"] [com.bhauman/rebel-readline "0.1.4"]]
-  :main ^:skip-aot clojure-exercises.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+                      :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :dependencies [[org.clojure/clojure "1.10.1"] [com.bhauman/rebel-readline "0.1.4"] [proto-repl "0.3.1"]]
+            :main ^:skip-aot clojure-exercises.core
+            :target-path "target/%s"
+            :profiles {:uberjar {:aot :all}})
